9d7ea2558c1bb1e1768c1df4fee096f8743177fc
fabricio.sobral <fabricio.pinto@infobase.com.br>
2023-05-23 00:59:09 -0300

    TASK-4-gql

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import CarouselSlider from './components/CarouselSlider/CarouselSlider'
+import CarouselSlider from './components/CarouselSlider/CarouselSlider';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
